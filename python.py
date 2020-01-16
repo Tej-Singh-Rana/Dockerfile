47910b992072c722395b9f0bbd666eace68fe22b
root <root@station185.example.com>
2020-01-16 19:31:16 +0530

    Back-end Page

---
+++ python.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import cgi, cgitb
+import os
+#mysql database connectivity
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="data",
+  user="root",
+  passwd="redhat123",
+  database="users"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("select * from login")
+myresult = mycursor.fetchall() # it gives the list
+
+# Create instance of FieldStorage
+print ("Content-Type: text/html\n")
+
+form = cgi.FieldStorage()
+
+# Get data from fields
+username = form.getvalue('Username')
+password  = form.getvalue('Password')
+
+
+for row in myresult:
+        if (username == row[0]) and (password == row[1]):
+                print("Content-Type: text/html\n")
+                print("welcome to this Official Page")
+
+        else:
+                print("Content-Type: text/html\n")
+                print("<h2>Username and Password Incorrect</h2>")
+
+
+mycursor.close()
